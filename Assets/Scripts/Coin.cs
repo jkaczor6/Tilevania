4d5dcbdf80ceaf1e7dd28ef522cf49ca805dd114
Jakub Kaczor <j.kaczor06@gmail.com>
2026-01-05 16:55:50 +0100

    added coin pickups

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    [SerializeField] AudioClip coinPickupSound;
+    bool wasCollected = false;
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Player"))
+        if(collision.CompareTag("Player") && !wasCollected)
         {
+            wasCollected = true;
             Destroy(gameObject);
+            gameObject.SetActive(false);
+            AudioSource.PlayClipAtPoint(coinPickupSound, transform.position);
         }
     }
 }
